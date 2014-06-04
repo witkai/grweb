3054b57523258f46bad2e095e8e04c6e0417dc19
kai <kai@test.com>
2014-06-04 17:47:43 -0400

    inital

---
+++ app/config.js
@@ -0,0 +1,14 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    bootstrap: "../bower_components/bootstrap/dist/js/bootstrap",
+    jquery: "../bower_components/jquery/dist/jquery",
+    modernizr: "../bower_components/modernizr/modernizr",
+    handlebars: "../bower_components/handlebars/handlebars"
+  },
+  packages: [
+
+  ]
+});
